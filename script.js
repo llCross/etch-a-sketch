1fc03a50d664c45332d0fff8b73c2d1d1782fba6
Cross <118707038+llCross@users.noreply.github.com>
2023-04-14 02:24:51 +0800

    Added a slider

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-const container = document.querySelector('.grid-container');
-
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
-}
+  const gridSizeSlider = document.getElementById('grid-size-slider');
+  const gridSizeLabel = document.getElementById('grid-size');
+  
+  gridSizeSlider.addEventListener('input', () => {
+    const gridSize = gridSizeSlider.value;
+    gridSizeLabel.textContent = `${gridSize}x${gridSize}`;
+    createGrid(gridSize);
+  });
+  
+  function createGrid(size) {
+    const container = document.querySelector('.grid-container');
+    container.innerHTML = '';
+    container.style.width = '750px';
+    container.style.height = '750px';
+  
+    const squareSize = 750 / size;
+  
+    for (let i = 0; i < size * size; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.style.width = `${squareSize}px`;
+      square.style.height = `${squareSize}px`;
+      container.appendChild(square);
+    }
+  }
+  
+  // initialize grid
+  createGrid(gridSizeSlider.value);
+  gridSizeLabel.textContent = `${gridSizeSlider.value}x${gridSizeSlider.value}`;
 
 });
